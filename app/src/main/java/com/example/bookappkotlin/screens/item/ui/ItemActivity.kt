4905c4c272aa783ee1e1248cf79bb97466fde51e
Gleidson Jonas <gleidsonjonas@hotmail.com>
2023-11-03 01:20:18 -0300

    Add: Product Card

---
+++ app/src/main/java/com/example/bookappkotlin/screens/item/ui/ItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookappkotlin.screens.item.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookappkotlin.R
+
+class ItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+    }
+}
